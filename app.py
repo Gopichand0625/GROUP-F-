cc1eab8b6c65fb234349ab172bc383051ad84117
GopiPinneti <138875425+GopiPinneti@users.noreply.github.com>
2023-07-16 03:24:37 +0100

    read the books table and displayed records on index page
    read the books table and displayed all the books on index page

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template,session
 from flask_sqlalchemy import SQLAlchemy
-
+from cs50 import SQL
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
+db = SQL ( "sqlite:///data.db" )
 
 @app.route("/")
 def index():
-    books = db.execute("SELECT * FROM books")
+    books = db.execute("select * FROM books")
     booksLen = len(books)
-    # Initialize variables
     shoppingCart = []
     shopLen = len(shoppingCart)
     totItems= 0
     total=0
     display=0
     if 'user' in session:
-        shoppingCart = db.execute("SELECT image, SUM(qty), SUM(subTotal), price, id FROM cart")
+        shoppingCart = db.execute("select image, SUM(qty), SUM(subTotal), price, id FROM cart")
         shopLen = len(shoppingCart)
         for i in range(shopLen):
             total += shoppingCart[i]["SUM(subTotal)"]
             totItems += shoppingCart[i]["SUM(qty)"]
-        books = db.execute("SELECT * FROM books")
+        books = db.execute("select * FROM books")
         booksLen = len(books)
         return render_template ("index.html", shoppingCart=shoppingCart, books=books, shopLen=shopLen, booksLen=booksLen, total=total, totItems=totItems, display=display, session=session )
     return render_template ( "index.html", books=books, shoppingCart=shoppingCart, booksLen=booksLen, shopLen=shopLen, total=total, totItems=totItems, display=display)
