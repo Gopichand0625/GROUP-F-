dcfa3e96471d9884acf9f1c05d6674d17f6e6a18
GopiPinneti <138875425+GopiPinneti@users.noreply.github.com>
2023-07-16 02:32:07 +0100

    created insert_data.py file to insert data into database
    created insert_data.py file to insert data into database

--- models.py
+++ models.py
@@ -3,7 +3,6 @@ from app import db
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.Text)
-    team = db.Column(db.Text)
     qty = db.Column(db.Integer)
     price = db.Column(db.Float)
     subTotal = db.Column(db.Float)
@@ -11,7 +10,6 @@ class Cart(db.Model):
 class Purchases(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uid = db.Column(db.Text)
-    team = db.Column(db.Text)
     image = db.Column(db.Text)
     quantity = db.Column(db.Integer)
     date = db.Column(db.Date)
