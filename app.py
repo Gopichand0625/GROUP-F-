b5918c926293ee78edc950dad02f2ed675577e03
Gopichand0625 <137396149+Gopichand0625@users.noreply.github.com>
2023-07-17 11:50:28 +0100

    added code to configure session

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template,session
 from flask_sqlalchemy import SQLAlchemy
 from cs50 import SQL
+from flask_session import Session
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 db = SQL ( "sqlite:///data.db" )
 
 @app.route("/")
