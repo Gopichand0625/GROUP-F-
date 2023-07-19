261c82f6ff5deeff6c56b214a893c6af61605c8e
Gopichand0625 <137396149+Gopichand0625@users.noreply.github.com>
2023-07-19 17:13:40 +0100

    made changes to handle the handler for buying and update item, added minor fixes for bugs

--- Static/JS/script.js
+++ Static/JS/script.js
@@ -1,117 +1,28 @@
-const SUBMIT = $( "#submit" );
 
-const USERNAME = $( "#uname" );
-const USERNAME_MSG = $( "#user-msg" );
+    $(".target").on("click", function () {
+        let $button = $(this);
+        let oldVal = parseInt($button.parent().find("input").val());
+        let newVal = 0;
 
-const PASSWORD = $( "#pwd" );
-const PASSWORD_MSG = $( "#password-msg" );
+        if ($button.text() == '+') {
+            newVal = oldVal + 1;
+        }
 
-const CONFIRM = $( "#confirmpwd" );
-const CONFIRM_MSG = $( "#confirm-msg" );
+        else {
+            if (oldVal > 0) {
+                newVal = oldVal - 1;
+            }
+            else {
+                newVal = 0;
+            }
+        }
 
-const FNAME = $( "#fname" );
-const FNAME_MSG = $( "#fname-msg" );
-
-const LNAME = $( "#lname" );
-const LNAME_MSG = $( "#lname-msg" );
-
-const EMAIL = $( "#email" );
-const EMAIL_MSG = $( "#email-msg" );
-
-function reset_form ( )
-{
-    USERNAME_MSG.html( "" );
-    USERNAME_MSG.hide();
-    PASSWORD_MSG.html( "" );
-    PASSWORD_MSG.hide();
-    CONFIRM_MSG.html( "" );
-    CONFIRM_MSG.hide();
-    LNAME_MSG.html( "" );
-    LNAME_MSG.hide();
-    FNAME_MSG.html( "" );
-    FNAME_MSG.hide();
-    EMAIL_MSG.html( "" );
-    EMAIL_MSG.hide();
-    SUBMIT.show();
-}
-
-function validate ( )
-{
-    let valid = true;
-    reset_form ( );
-    SUBMIT.hide();
-
-    
-    if ( !USERNAME.val() || USERNAME.val().length < 5  )
-    {
-        
-        USERNAME_MSG.html( "Username must be of atleast five characters" );
-        USERNAME_MSG.show();
-        console.log( "Bad username" );
-        valid = false;
-    }
+        $button.parent().find("input").val(newVal);
+    });
 
 
-    if ( USERNAME.val() != USERNAME.val().toLowerCase())
-    {
-        USERNAME_MSG.html("Username must be in lowercase");
-        USERNAME_MSG.show();
-        valid = false;
-    }
-
-    if ( !PASSWORD.val() || PASSWORD.val().length < 8 )
-    {
-        PASSWORD_MSG.html("Password must be at least eight characters long");
-        PASSWORD_MSG.show();
-        valid = false;
-    }
-
-    if ( !CONFIRM.val() || PASSWORD.val() != CONFIRM.val() )
-    {
-        CONFIRM_MSG.html("Enetered Passwords donot match");
-        CONFIRM_MSG.show();
-        valid = false;
-    }
-
-    if ( !FNAME.val() )
-    {
-        FNAME_MSG.html("First Name cannot be empty");
-        FNAME_MSG.show();
-        valid = false;
-    }
-
-    if ( !LNAME.val() )
-    {
-        LNAME_MSG.html("Last Name cannot be empty");
-        LNAME_MSG.show();
-        valid = false;
-    }
-
-    var x = EMAIL.val().trim();
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
-    if ( atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length ) {
-        EMAIL_MSG.html("Enter a valid email address");
-        EMAIL_MSG.show();
-        valid = false;
-    }
-
-    if ( valid )
-    {
-        reset_form ( );
-    }
-}
-
-$(document).ready ( validate );
-USERNAME.change ( validate );
-PASSWORD.change ( validate );
-CONFIRM.change ( validate );
-LNAME.change ( validate );
-FNAME.change ( validate );
-EMAIL.change ( validate );
-
-$('.addToCart').on("click", function(event) {
-    if($(this).prev().prev().prev().find("input").val() == '0') {
+$('.addToCart').on("click", function (event) {
+    if ($(this).prev().prev().prev().find("input").val() == '0') {
         event.preventDefault();
         $(this).next().next().next().html("please select at least one book");
         $(this).next().next().next().css("display", "block");
@@ -119,37 +30,11 @@ $('.addToCart').on("click", function(event) {
     }
 
     if ($(this).prev().val() == "0") {
-            event.preventDefault();
-            $(this).next().next().next().html("Please log in order to purchase");
-            $(this).next().next().next().css("display", "block");
-            $(this).next().next().next().delay(3000).slideUp();
-        }
+        event.preventDefault();
+        $(this).next().next().next().html("Please log in order to purchase");
+        $(this).next().next().next().css("display", "block");
+        $(this).next().next().next().delay(3000).slideUp();
+    }
 });
 
 $(".flashMessage").delay(3000).slideUp();
-
-$(".target").on("click", function() {
-    let $button = $(this);
-    let oldVal = parseInt($button.parent().find("input").val());
-    let newVal = 0;
-
-    if ($button.text() == '+') {
-        newVal = oldVal + 1;
-    }
-
-    else {
-        if (oldVal > 0) {
-            newVal = oldVal - 1;
-        }
-        else {
-            newVal = 0;
-        }
-    }
-
-    $button.parent().find("input").val(newVal);
-});
-
-$(window).load(function(){
-//pass
-})
-
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template,session, redirect,request
 from cs50 import SQL
 from flask_session import Session
+from flask import Flask, render_template,session, redirect,request
 from datetime import datetime
+import locale
+
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
@@ -36,6 +38,10 @@ def login():
 
 @app.route("/signup/", methods=["GET"])
 def signup():
+    return render_template("signup.html")
+
+@app.route("/register/", methods=["GET"])
+def registration():
     uname = request.form["uname"]
     pwd = request.form["pwd"]
     fname = request.form["fname"]
@@ -43,7 +49,7 @@ def signup():
     email = request.form["email"]
     rows = db.execute( "SELECT * FROM users WHERE username = :username ", username = uname )    
     if len( rows ) > 0:
-        return render_template ( "new.html", msg="Username already exists!" )    
+        return render_template ( "signup.html", msg="Username already exists!" )    
     new = db.execute ( "INSERT INTO users (username, password, fname, lname, email) VALUES (:uname, :pwd, :fname, :lname, :email)",
                     username=uname, password=pwd, fname=fname, lname=lname, email=email )    
     return render_template ( "login.html" )
@@ -108,6 +114,97 @@ def remove():
      return render_template ("cart.html", shoppingCart=shoppingCart, shopLen=shopLen, total=total, totItems=totItems, display=display, session=session)
 
 
+@app.route("/filter/")
+def filter():
+    if request.args.get('sale'):
+        query = request.args.get('sale')
+        books = db.execute("SELECT * FROM books WHERE onSale = :query", query=query)
+    
+    if request.args.get('kind'):
+        query = request.args.get('kind')
+        books = db.execute("SELECT * FROM books WHERE kind = :query", query=query)
+        
+    if request.args.get('price'):
+        query = request.args.get('price')
+        books = db.execute("SELECT * FROM books")
+        
+    booksLen = len(books)
+    
+    shoppingCart = []
+    shopLen = len(shoppingCart)
+    totItems, total, display = 0, 0, 0
+    if 'user' in session:
+        
+        shoppingCart = db.execute("SELECT image, SUM(qty), SUM(subTotal), price, id FROM cart")
+        shopLen = len(shoppingCart)
+        for i in range(shopLen):
+            total += shoppingCart[i]["SUM(subTotal)"]
+            totItems += shoppingCart[i]["SUM(qty)"]
+        
+        return render_template ("index.html", shoppingCart=shoppingCart, books=books, shopLen=shopLen, booksLen=booksLen, total=total, totItems=totItems, display=display, session=session )
+    
+    return render_template ( "index.html", books=books, shoppingCart=shoppingCart, booksLen=booksLen, shopLen=shopLen, total=total, totItems=totItems, display=display)
+
+
+@app.route("/buy/")
+def buy():
+    shoppingCart = []
+    shopLen = len(shoppingCart)
+    totItems, total, display = 0, 0, 0
+    qty = int(request.args.get('quantity'))
+    if session:
+        id = int(request.args.get('id'))
+        goods = db.execute("SELECT * FROM books WHERE id = :id", id=id)
+        
+        if(goods[0]["onSale"] == 1):
+            price = goods[0]["onSalePrice"]
+        else:
+            price = goods[0]["price"]
+        image = goods[0]["image"]
+        subTotal = qty * price
+        db.execute("INSERT INTO cart (id, qty, image, price, subTotal) VALUES (:id, :qty, :image, :price, :subTotal)", id=id, qty=qty, image=image, price=price, subTotal=subTotal)
+        shoppingCart = db.execute("SELECT image, SUM(qty), SUM(subTotal), price, id FROM cart")
+        shopLen = len(shoppingCart)
+        for i in range(shopLen):
+            total += shoppingCart[i]["SUM(subTotal)"]
+            totItems += shoppingCart[i]["SUM(qty)"]
+        books = db.execute("SELECT * FROM books")
+        booksLen = len(books)
+     
+        return render_template ("index.html", shoppingCart=shoppingCart, books=books, shopLen=shopLen, booksLen=booksLen, total=total, totItems=totItems, display=display, session=session )
+
+
+@app.route("/update/")
+def update():
+    shoppingCart = []
+    shopLen = len(shoppingCart)
+    totItems, total, display = 0, 0, 0
+    qty = int(request.args.get('quantity'))
+    if session:
+        id = int(request.args.get('id'))
+        db.execute("DELETE FROM cart WHERE id = :id", id=id)
+     
+        bookitems = db.execute("SELECT * FROM books WHERE id = :id", id=id)
+       
+        if(bookitems[0]["onSale"] == 1):
+            price = bookitems[0]["onSalePrice"]
+        else:
+            price = bookitems[0]["price"]
+        team = bookitems[0]["team"]
+        image = bookitems[0]["image"]
+        subTotal = qty * price
+       
+        db.execute("INSERT INTO cart (id, qty,image, price, subTotal) VALUES (:id, :qty,:image, :price, :subTotal)", id=id, qty=qty, image=image, price=price, subTotal=subTotal)
+        shoppingCart = db.execute("SELECT image, SUM(qty), SUM(subTotal), price, id FROM cart")
+        shopLen = len(shoppingCart)
+   
+        for i in range(shopLen):
+            total += shoppingCart[i]["SUM(subTotal)"]
+            totItems += shoppingCart[i]["SUM(qty)"]
+       
+        return render_template ("cart.html", shoppingCart=shoppingCart, shopLen=shopLen, total=total, totItems=totItems, display=display, session=session )
+
 
 if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
     app.run()
